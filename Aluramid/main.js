27d00026012d9b68ee57a59cae16aa3e84be3a5d
Ewerton Ds <ewerton.eweds@gmail.com>
2023-05-10 01:25:31 -0300

    update Aluramidi #02

--- Aluramid/main.js
+++ Aluramid/main.js
@@ -1,7 +1,15 @@
 //alert('TA FUNCIONANDO');
+const tecladoMidi = document.querySelectorAll('.tecla');
 
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+for(let i=0;i<tecladoMidi.length;i+=1){
+    const tecla = tecladoMidi[i];
+    const instrumento = tecla.classList[1]; //referenciou o elemento 1 da lista de classes do objeto
+    const idAudio = `#som_${instrumento}`; //template string
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
+}
